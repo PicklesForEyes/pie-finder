cd8140f8a50bca89c515d6ac6edee9c6b897304a
Benjamin Lawson <lawbe101@yahoo.com>
2017-11-29 16:09:16 -0500

    update port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path')
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var html = require('./app/routing/htmlRoutes.js');
 var api = require('./app/routing/apiRoutes.js');
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.listen(port, () => console.log(`Listening on port: ${port}`));
 
 app.use('/', html);
 app.use('/api/', api);

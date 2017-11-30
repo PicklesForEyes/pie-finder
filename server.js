233c6d71e74c55e1cd277e8634dda158e9cb8cdf
Benjamin Lawson <lawbe101@yahoo.com>
2017-11-30 00:18:35 -0500

    updated static files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/app/public/'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var html = require('./app/routing/htmlRoutes.js');
